758614553d8ed7b36a1bff4609819a30db209b0f
Cameron <54632731+Fable-git@users.noreply.github.com>
2020-01-19 21:46:39 -0500

    Re-added Spoofing

--- src/BestBotv3/HQ.java
+++ src/BestBotv3/HQ.java
@@ -22,6 +22,12 @@ public class HQ extends Shooter {
                 }
             }
         }
+
+        //Every 3 turns repeat messages
+        if(turnCount % 3 == 0){
+            comms.jamEnemyComms();
+        }
+
         if(numMiners < 6) {
             for (Direction dir : Util.directions)
                 if(tryBuild(RobotType.MINER, dir)){
